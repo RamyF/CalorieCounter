187ee6dfddc7294a530e45d30c0dfaac01cff41f
Ramy Farah <ramyf21@gmail.com>
2019-07-30 04:02:23 -0600

    Added pie chart on home view with static data

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
-class HomesView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+  State<StatefulWidget> createState() {
+    return HomeViewState();
+  }
+}
+
+class HomeViewState extends State<HomeView> {
+
+  List<charts.Series<Calories, String>> _seriesPieData;
+
+
+
+
+  _generateData() {
+    // dummy data for chart
+    var pieData = [
+      Calories('Consumed', 2100, Colors.lightBlue),
+      Calories('Left', 300, Colors.red),
+    ];
+
+    // pie data added to series
+    _seriesPieData.add(
+      charts.Series(
+        data: pieData,
+        domainFn: (Calories calories, _) => calories.name,
+        measureFn: (Calories calories, _) => calories.value,
+        colorFn: (Calories calories, _) => charts.ColorUtil.fromDartColor(calories.colorValue),
+        id: 'Calorie Balance',
+        labelAccessorFn: (Calories row, _)=>'${row.value} cals',
+      ),
     );
   }
+
+
+
+    @override
+    void initState() {
+      super.initState();
+      _seriesPieData = List<charts.Series<Calories, String>>();
+      _generateData();
+    }
+
+
+
+
+    @override
+    Widget build(BuildContext context) {
+      return Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Container(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text('Daily Caloric Intake', style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),),
+                SizedBox(height: 10.0,),
+                Expanded(
+                  child: charts.PieChart(
+                    _seriesPieData,
+                    animate: true,
+                    animationDuration: Duration(seconds: 2),
+                    defaultRenderer: charts.ArcRendererConfig(
+                      arcWidth: 75,
+                      arcRendererDecorators: [
+                        charts.ArcLabelDecorator(
+                          labelPosition: charts.ArcLabelPosition.outside)
+                      ]),
+                  )
+                ),
+              ],),
+          ),
+        ),
+      );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Calories{
+  String name;
+  int value;
+  Color colorValue;
+
+  Calories(this.name, this.value, this.colorValue);
 }
--- lib/views/view_selection.dart
+++ lib/views/view_selection.dart
@@ -3,62 +3,59 @@ import 'home_view.dart';
 import 'add_view.dart';
 import 'history_view.dart';
 
-// responsible for showing different views based on bottom nav bar selection 
+// responsible for showing different views based on bottom nav bar selection
 
-class ViewSelection extends StatefulWidget{
+class ViewSelection extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _ViewSelectionState();
   }
 }
 
-
-class _ViewSelectionState extends State<ViewSelection>{
+class _ViewSelectionState extends State<ViewSelection> {
   // allows app to navigate between 3 choices
   int _currentIndex = 0;
   // different views
   final List<Widget> _views = [
-    HomesView(),
+    HomeView(),
     AddView(),
     HistoryView(),
   ];
 
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Calorie Counter'),
-        ),
-        // app body that displays views based on menu selection
-        body: _views[_currentIndex],
-        // bottom nav bar
-        bottomNavigationBar: BottomNavigationBar(
-          onTap: onTabTapped,
-          currentIndex: _currentIndex,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text("Home"),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.edit),
-              title: Text("Add"),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.history),
-              title: Text("History"),
-            ),
-          ],
-        ),
-      );
+      appBar: AppBar(
+        title: Text('Calorie Counter'),
+      ),
+      // app body that displays views based on menu selection
+      body: _views[_currentIndex],
+      // bottom nav bar
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.edit),
+            title: Text("Add"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            title: Text("History"),
+          ),
+        ],
+      ),
+    );
   }
 
   // sets which view to be displayed
   void onTabTapped(int index) {
     setState(() {
-     _currentIndex = index; 
+      _currentIndex = index;
     });
   }
 }
-
