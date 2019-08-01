b3fd0a8e8a71a01a3833962be736cdd3faac1ad7
Ramy Farah <ramyf21@gmail.com>
2019-08-01 04:15:30 -0600

    Repositioned PieChart & added card to HomeView

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,99 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
+import '../widgets/pie_chart.dart';
 
-class HomeView extends StatefulWidget {
+class HomeView extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    return HomeViewState();
-  }
-}
+  Widget build(BuildContext context) {
+    double deviceHeight = MediaQuery.of(context).size.height;
 
-class HomeViewState extends State<HomeView> {
-
-  List<charts.Series<Calories, String>> _seriesPieData;
-
-
-
-
-  _generateData() {
-    // dummy data for chart
-    var pieData = [
-      Calories('Consumed', 2100, Colors.lightBlue),
-      Calories('Left', 300, Colors.red),
-    ];
-
-    // pie data added to series
-    _seriesPieData.add(
-      charts.Series(
-        data: pieData,
-        domainFn: (Calories calories, _) => calories.name,
-        measureFn: (Calories calories, _) => calories.value,
-        colorFn: (Calories calories, _) => charts.ColorUtil.fromDartColor(calories.colorValue),
-        id: 'Calorie Balance',
-        labelAccessorFn: (Calories row, _)=>'${row.value} cals',
-      ),
-    );
-  }
-
-
-
-    @override
-    void initState() {
-      super.initState();
-      _seriesPieData = List<charts.Series<Calories, String>>();
-      _generateData();
-    }
-
-
-
-
-    @override
-    Widget build(BuildContext context) {
-      return Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Container(
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Text('Daily Caloric Intake', style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),),
-                SizedBox(height: 10.0,),
-                Expanded(
-                  child: charts.PieChart(
-                    _seriesPieData,
-                    animate: true,
-                    animationDuration: Duration(seconds: 2),
-                    defaultRenderer: charts.ArcRendererConfig(
-                      arcWidth: 75,
-                      arcRendererDecorators: [
-                        charts.ArcLabelDecorator(
-                          labelPosition: charts.ArcLabelPosition.outside)
-                      ]),
-                  )
-                ),
-              ],),
+    return Column(
+      children: <Widget>[
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text('Summary', style: TextStyle(fontSize: 30)),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: Align(
+            alignment: Alignment.topCenter,
+                child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: (deviceHeight * .40),
+              child: PieChart(),
+            ),
           ),
         ),
-      );
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Calories{
-  String name;
-  int value;
-  Color colorValue;
-
-  Calories(this.name, this.value, this.colorValue);
+        Expanded(
+          child: Card(
+            elevation: 20,
+            child: Center(
+              child: Row(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text('2500'),
+                      Text('Goal')
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ),
+        ),
+      ],
+    );
+  }
 }
---
+++ lib/widgets/pie_chart.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class PieChart extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return PieChartState();
+  }
+}
+
+class PieChartState extends State<PieChart> {
+  List<charts.Series<Calories, String>> _seriesPieData;
+
+  _generateData() {
+    // dummy data for chart
+    var pieData = [
+      Calories('Consumed', 2100, Colors.lightBlue),
+      Calories('Left', 300, Colors.red),
+    ];
+
+    // pie data added to series
+    _seriesPieData.add(
+      charts.Series(
+        data: pieData,
+        domainFn: (Calories calories, _) => calories.name,
+        measureFn: (Calories calories, _) => calories.value,
+        colorFn: (Calories calories, _) =>
+            charts.ColorUtil.fromDartColor(calories.colorValue),
+        id: 'Calorie Balance',
+        labelAccessorFn: (Calories row, _) => '${row.value} cals',
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _seriesPieData = List<charts.Series<Calories, String>>();
+    _generateData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: charts.PieChart(
+            _seriesPieData,
+            animate: true,
+            animationDuration: Duration(seconds: 2),
+            defaultRenderer: charts.ArcRendererConfig(
+                arcWidth: 75,
+                arcRendererDecorators: [
+                  charts.ArcLabelDecorator(
+                      labelPosition: charts.ArcLabelPosition.outside)
+                ]),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Calories {
+  String name;
+  int value;
+  Color colorValue;
+
+  Calories(this.name, this.value, this.colorValue);
+}
